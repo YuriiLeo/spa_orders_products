b63fc4c7d492cb911a54b723d00a17005a95bffb
YuriiLeo <yuriileontenko@gmail.com>
2024-08-02 14:52:12 +0200

    refactor: moved pages to folder

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,8 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import AppEntrypoint from "./providers/StoreProvider";
 import ErrorPage from "./error-page";
 import { RouterProvider } from "react-router-dom";
-import Orders from "./components/Orders/Orders";
-import Products from "./components/Products/Products";
+import Orders from "./pages/Orders";
+import Products from "./pages/Products";
 
 const router = createBrowserRouter([
   {
